ed58172e119727e3092d30aa1f98608345dc840a
Hozaifa Owaisi <hozaifaowaisi.h@gmail.com>
2019-11-23 12:32:41 -0500

    secound Commit

---
+++ Unit-Conversion-Toolkit/UI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Unit_Conversion_Toolkit
+{
+    public class UI
+    {
+        private string typeOfUnit;
+        private string initalUnit;
+        private string convortToUnit;
+        private double numberToCovort;
+
+        public UI(string typeOfUnit_l,string initalUnit_l,string convortToUnit_l,double numberToConcort_l)
+        {
+            typeOfUnit = typeOfUnit_l;
+            initalUnit = initalUnit_l;
+            convortToUnit = convortToUnit_l;
+            numberToCovort = numberToConcort_l;
+
+        }
+
+    }
+}
